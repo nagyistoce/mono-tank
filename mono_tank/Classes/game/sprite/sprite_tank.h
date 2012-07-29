501a4882edbee7a7889f74b875ba6b208e2e31ae
tonysyata <tonysyata@10f37062-bd6e-bffd-f1b1-fa8ce5944ac1>
2012-07-29 14:40:05 +0000

    添加 graphic.h:渲染数据 sprite_tank:坦克````

---
+++ mono_tank/Classes/game/sprite/sprite_tank.h
@@ -0,0 +1,18 @@
+#ifndef sprite_tank_h
+#define sprite_tank_h
+
+#include "head.h"
+
+class CSpriteTank : cocos2d::CCSprite
+{
+public:
+	CSpriteTank();
+	virtual ~CSpriteTank();
+
+	static CSpriteTank *instance();
+
+	bool initSpriteTank();
+	virtual void draw();
+};
+
+#endif
--- mono_tank/Classes/main/handle.h
+++ mono_tank/Classes/main/handle.h
@@ -10,15 +10,6 @@ extern cocos2d::CCPoint g_ScrrenCenter;
 
 extern CSceneGame *g_SceneGame;
 
-/*graphic data
-*/
-const GLfloat g_PlayerVertex[] = {-25.0f, -0.0f, 25.0f, -0.0f, 0.0f, 50.0f};
-const GLubyte g_PlayerIndex[] = {0, 1, 1, 2, 2, 0};
-
-const GLfloat g_CtrlRightVertex[] = {0.0f, -15.0f, 30.0f, 0.0f, 0.0f, 15.0f};
-const GLubyte g_CtrlRightIndexNormal[] = {0, 1, 1, 2, 2, 0};
-const GLubyte g_CtrlRightIndexSelected[] = {0, 1, 2};
-
 void AppSetup();
 
 #endif
